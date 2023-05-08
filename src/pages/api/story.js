699534a50d7084322390e1628e8cf6ead5d0c7ff
chadluo <i@yuki.rocks>
2023-05-08 23:57:29 +1000

    kv for api

--- src/pages/api/story.js
+++ src/pages/api/story.js
@@ -1,3 +1,4 @@
+import kv from "@vercel/kv";
 import { NextResponse } from "next/server";
 import { parse } from "parse5";
 
@@ -7,28 +8,36 @@ export const config = {
 
 export default async function handler(request) {
   const storyId = new URL(request.url).searchParams.get("storyId");
-  const story = await (await fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)).json();
-  const [meta] = await Promise.all([findMetadata(storyId, story.url)]);
-  return NextResponse.json({
-    story,
-    meta,
-  });
+  if (!storyId) {
+    return NextResponse.error();
+  }
+
+  const lastUpdate = new Date().getTime();
+  const existingStory = await kv.get(storyId);
+  if (existingStory && existingStory.lastUpdate > lastUpdate - 1000 * 60 * 60 * 2) {
+    return NextResponse.json(existingStory);
+  } else {
+    const hnStory = await (await fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)).json();
+    const [meta] = await Promise.all([findMetadata(storyId, hnStory.url)]);
+    const newStory = { story: hnStory, meta, lastUpdate };
+    kv.set(storyId, newStory);
+    return NextResponse.json(newStory);
+  }
 }
 
 const DEFAULT_TIMEOUT_MS = 5000;
 
 async function findMetadata(storyId, url) {
+  if (url.toLowerCase().endsWith("pdf")) {
+    return {};
+  }
   const controller = new AbortController();
   let html, abortTimeout;
   try {
     abortTimeout = setTimeout(() => controller.abort(), DEFAULT_TIMEOUT_MS);
     html = await fetch(url, { signal: controller.signal }).then((response) => response.text());
   } catch (err) {
-    console.error({
-      error: "Cannot fetch html " + err,
-      storyId,
-      url,
-    });
+    console.error({ error: "Cannot fetch html " + err, storyId, url });
     return {};
   }
   clearTimeout(abortTimeout);
