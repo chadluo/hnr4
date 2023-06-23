c094e5bab94bce6b87ea8f8ce202278661e81029
chad luo <i@yuki.rocks>
2023-06-24 01:42:31 +1000

    icon

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -12,15 +12,30 @@ type CardProps = {
   description?: string;
 };
 
+export type Website = "apple" | "github" | "reddit" | "wikipedia" | "youtube";
+
 export default function Card(props: CardProps) {
   const { title, url, image, authors, description } = props;
 
   const source = authors ? authors : url && extractSource(url);
+  const website = findWebsite(url);
+  const icon = website ? (
+    <>
+      {" "}
+      <i className={classNames("fa", `fa-${mapIcon(website)}`)}></i>{" "}
+    </>
+  ) : undefined;
 
   return (
-    <Link href={url} title={url} className={classNames(styles.card, { [styles.imageCard]: image })} target="_blank" >
+    <Link
+      href={url}
+      title={url}
+      className={classNames(styles.card, { [styles.imageCard]: image })}
+      target="_blank"
+    >
       <div className={styles.textBox}>
         <span className={styles.source} title={url}>
+          {icon}
           {source}
         </span>
         <span className={styles.title}>{title}</span>
@@ -28,7 +43,12 @@ export default function Card(props: CardProps) {
           {description}
         </div>
       </div>
-      {image && <div className={styles.imageBox} style={{ backgroundImage: `url(${image})` }}></div>}
+      {image && (
+        <div
+          className={styles.imageBox}
+          style={{ backgroundImage: `url(${image})` }}
+        ></div>
+      )}
     </Link>
   );
 }
@@ -41,6 +61,36 @@ function extractSource(url: string) {
     return hostname + takePath(pathname, 2);
   } else {
     return hostname.replace(/^www\./, "");
+  }
+}
+
+function findWebsite(url: string): Website | undefined {
+  const { hostname } = new URL(url);
+  if (hostname.endsWith("apple.com")) {
+    return "apple";
+  } else if (hostname.endsWith("github.com")) {
+    return "github";
+  } else if (hostname.endsWith("reddit.com")) {
+    return "reddit";
+  } else if (hostname.endsWith("wikipedia.org")) {
+    return "wikipedia";
+  } else if (hostname.endsWith("youtube.com")) {
+    return "youtube";
+  } else {
+    return undefined;
+  }
+}
+
+function mapIcon(icon: Website) {
+  switch (icon) {
+    case "reddit":
+      return "reddit-alien";
+    case "wikipedia":
+      return "wikipedia-w";
+    case "youtube":
+      return "youtube-play";
+    default:
+      return icon;
   }
 }
 
--- src/app/story/[id]/page.tsx
+++ src/app/story/[id]/page.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
 import Card from "@/app/card";
 import Comment from "@/app/comment";
 import Footer from "@/app/footer";
 import styles from "@/styles/index.module.css";
-import storyPage from '@/styles/storyPage.module.css';
+import storyPage from "@/styles/storyPage.module.css";
 import classNames from "classnames";
 import { IBM_Plex_Mono, IBM_Plex_Sans } from "next/font/google";
 import Link from "next/link";
@@ -33,42 +33,53 @@ type Summary = {
 type Props = { params: { id: string } };
 
 const mono = IBM_Plex_Mono({ weight: "400", subsets: ["latin"] });
-const sans = IBM_Plex_Sans({ weight: ["400", "700"], subsets: ["latin"] })
+const sans = IBM_Plex_Sans({ weight: ["400", "700"], subsets: ["latin"] });
 
 export default function Page({ params }: Props) {
-
   const [hnStory, setHnStory] = React.useState<HNStory>();
   const [meta, setMeta] = React.useState<Meta>();
-  const [summary, setSummary] = React.useState<Summary>({ short: "", long: "" });
+  const [summary, setSummary] = React.useState<Summary>({
+    short: "",
+    long: "",
+  });
   const [embedTweet, setEmbedTweet] = React.useState();
 
   React.useEffect(() => {
     const controller = new AbortController();
     (async (storyId: string) => {
       const hnStory: HNStory = await (
-        await fetch(`/api/story?storyId=${storyId}`, { signal: controller.signal })
+        await fetch(`/api/story?storyId=${storyId}`, {
+          signal: controller.signal,
+        })
       ).json();
       setHnStory(hnStory);
       if (!hnStory.url) return;
       const { hostname } = new URL(hnStory.url);
       if (hostname === "twitter.com") {
         fetch(`/api/tweet?url=${hnStory.url}`, { signal: controller.signal })
-          .then(response => response.json())
-          .then(json => setEmbedTweet(json.html));
+          .then((response) => response.json())
+          .then((json) => setEmbedTweet(json.html));
       } else {
         fetch(`/api/meta?url=${hnStory.url}`, { signal: controller.signal })
           .then((response) => response.json())
           .then(setMeta);
         if (hnStory.type !== "job") {
-          fetch(`/api/summary?storyId=${storyId}&url=${hnStory.url}`, { signal: controller.signal })
+          fetch(`/api/summary?storyId=${storyId}&url=${hnStory.url}`, {
+            signal: controller.signal,
+          })
             .then((response) => response.json())
             .then(setSummary)
             .catch((error) => {
-              console.error(`Failed getting summary for [${hnStory.url}]`, error);
+              console.error(
+                `Failed getting summary for [${hnStory.url}]`,
+                error
+              );
             });
         }
       }
-    })(params.id).catch((err) => console.error(`failed fetching story ${params.id}`, err));
+    })(params.id).catch((err) =>
+      console.error(`failed fetching story ${params.id}`, err)
+    );
 
     return () => {
       controller.abort();
@@ -95,7 +106,6 @@ export default function Page({ params }: Props) {
       <Card title={hnStory.title} url={hnStory.url || hnUrl} />
     ));
 
-
   return (
     <>
       <style jsx global>{`
@@ -106,18 +116,33 @@ export default function Page({ params }: Props) {
       `}</style>
       <header className={classNames(styles.header, storyPage.header)}>
         <h1 className={storyPage.title}>
-          <Link href="/" className={storyPage.back}>🡨</Link>
-          <Link href={hnUrl} className={classNames(storyPage.hnTitle, sans.className)} target="_blank">{hnStory?.title}</Link>
+          <Link href="/" className={storyPage.back}>
+            🡨
+          </Link>
+          <Link
+            href={hnUrl}
+            className={classNames(storyPage.hnTitle, sans.className)}
+            target="_blank"
+          >
+            {hnStory?.title}
+          </Link>
         </h1>
       </header>
-      <section className={classNames(styles.article, sans.className)}>
+      <section className={classNames(storyPage.article, sans.className)}>
         <div className={storyPage.story}>
-          <span className={classNames(storyPage.summary, mono.className)}>{summary.long}</span>
+          <span className={classNames(storyPage.summary, mono.className)}>
+            {summary.long}
+          </span>
           {card}
         </div>
         {(storyText || kids) && (
           <div className={storyPage.discussions}>
-            {storyText && <div className={storyPage.storyText} dangerouslySetInnerHTML={{ __html: storyText }}></div>}
+            {storyText && (
+              <div
+                className={storyPage.storyText}
+                dangerouslySetInnerHTML={{ __html: storyText }}
+              ></div>
+            )}
             {kids?.map((kid) => (
               <Comment key={kid} commentId={kid.toString()} expand={false} />
             ))}
