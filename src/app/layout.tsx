b0c53405c498ec50d93a664512746a7c28f28705
Chad Luo <i@yuki.rocks>
2023-11-07 00:52:30 +1100

    metadatabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
-import Script from 'next/script';
+import Script from "next/script";
 
 const title = "Hacker News Reader";
 const description =
   "Yet another Hacker News Reader with metadata cards and some LLM summaries.";
-const image = "https://hnr.adluo.ch/hnr.png";
+const image = "/hnr.png";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://hnr.adluo.ch"),
   title,
   description,
   openGraph: {
@@ -23,7 +24,11 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode; }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [deg1, hue1] = [Math.random() * 360, Math.random() * 360];
   const [deg2, hue2] = [Math.random() * 360, Math.random() * 360];
   const background = [
