bc3d7a1ecb168231e4c448bbe870d1712c79328d
Chad Luo <i@yuki.rocks>
2024-03-19 21:57:38 +1100

    tw config to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { Config } from "tailwindcss";
+
+const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+module.exports = config;
