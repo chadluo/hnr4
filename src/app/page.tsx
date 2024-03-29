8fe859ce0ec2000ad060b588694609d6207a0599
Chad Luo <i@yuki.rocks>
2024-03-29 22:15:17 +1100

    story placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import Footer from "./footer";
 import { getHNTopStories } from "./hn";
 import Story from "./story";
+import { StoryPlaceholder } from "./storyPlaceholder";
 
 export default async function Home() {
   const storyIds = await getHNTopStories();
@@ -15,7 +16,7 @@ export default async function Home() {
       </header>
       <main className="mx-auto flex w-5/6 min-w-64 max-w-6xl flex-col gap-6">
         {storyIds.map((storyId) => (
-          <Suspense key={storyId}>
+          <Suspense key={storyId} fallback={<StoryPlaceholder />}>
             <Story storyId={storyId} full={false} />
           </Suspense>
         ))}
