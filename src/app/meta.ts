09a53582436f36978436ba6487e1ec3859798a3a
Chad Luo <i@yuki.rocks>
2024-11-14 23:11:29 +1100

    turn off postgres

--- src/app/meta.ts
+++ src/app/meta.ts
@@ -1,4 +1,4 @@
-import { sql } from "@vercel/postgres";
+// import { sql } from "@vercel/postgres";
 import { parse } from "parse5";
 import type { Attribute } from "parse5/dist/common/token";
 import type {
@@ -16,11 +16,11 @@ export type Meta = {
 };
 
 export async function getMeta(storyId: number, html: string) {
-  const existingMeta =
-    await sql`select * from metadata where storyId = ${storyId}`;
-  if (existingMeta.rows.length > 0) {
-    return existingMeta.rows[0];
-  }
+  //  const existingMeta =
+  //    await sql`select * from metadata where storyId = ${storyId}`;
+  //  if (existingMeta.rows.length > 0) {
+  //    return existingMeta.rows[0];
+  //  }
   const rawMeta = findRawMeta(html);
   const meta = {
     title: (rawMeta.get("title") ??
@@ -35,26 +35,26 @@ export async function getMeta(storyId: number, html: string) {
     authors: rawMeta.get("citation_author")?.join(" | "),
   };
 
-  await sql`MERGE INTO metadata as target
-    using (values (${storyId},
-    ${meta.title?.slice(0, 255)},
-    ${meta.image?.slice(0, 255)},
-    ${meta.imageAlt?.slice(0, 255)},
-    ${meta.authors?.slice(0, 255)},
-    ${meta.description?.slice(0, 1024)})) as source (storyId, title, image, imageAlt, authors, description)
-    on target.storyId = source.storyId::int
-    when matched then
-      update set
-        added = ${new Date().toISOString()},
-        title = source.title,
-        image = source.image,
-        imageAlt = source.imageAlt,
-        authors = source.authors,
-        description = source.description
-    when not matched then
-      insert (storyId, added, title, image, imageAlt, authors, description)
-      values (source.storyId::int, ${new Date().toISOString()}, source.title, source.image, source.imageAlt, source.authors, source.description)
-    `;
+  //  await sql`MERGE INTO metadata as target
+  //    using (values (${storyId},
+  //    ${meta.title?.slice(0, 255)},
+  //    ${meta.image?.slice(0, 255)},
+  //    ${meta.imageAlt?.slice(0, 255)},
+  //    ${meta.authors?.slice(0, 255)},
+  //    ${meta.description?.slice(0, 1024)})) as source (storyId, title, image, imageAlt, authors, description)
+  //    on target.storyId = source.storyId::int
+  //    when matched then
+  //      update set
+  //        added = ${new Date().toISOString()},
+  //        title = source.title,
+  //        image = source.image,
+  //        imageAlt = source.imageAlt,
+  //        authors = source.authors,
+  //        description = source.description
+  //    when not matched then
+  //      insert (storyId, added, title, image, imageAlt, authors, description)
+  //      values (source.storyId::int, ${new Date().toISOString()}, source.title, source.image, source.imageAlt, source.authors, source.description)
+  //    `;
 
   return meta;
 }
